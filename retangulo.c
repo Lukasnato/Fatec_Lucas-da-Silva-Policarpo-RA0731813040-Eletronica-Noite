26eb087b52ecfcd79a4929ad829b7a478c85ec15
Lukasnato <69984710+Lukasnato@users.noreply.github.com>
2020-09-25 20:05:47 -0300

    Exercicio retangulo em C

---
+++ retangulo.c
@@ -0,0 +1,19 @@
+
+//cálculo área do retângulo
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  float e_lat1, e_lat2,s_area;
+
+  scanf("%f",&e_lat1);
+  scanf("%f",&e_lat2);
+
+  s_area= e_lat1*e_lat2;
+
+printf("area do retângulo:%f\n",s_area);
+
+return 0;
+
+}
